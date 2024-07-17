eb135f497289d702b968796a97d111d1d94e494a
isaac <fano.isaac17@gmail.com>
2024-07-17 18:04:37 +0200

    prova

---
+++ core/src/main/java/change/it/Runner.java
@@ -0,0 +1,13 @@
+package change.it;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Runner x = new Runner();
+		System.out.println("ciaooo");
+	}
+
+	public int somma(int x, int y) {
+		return x + y;
+	}
+}
