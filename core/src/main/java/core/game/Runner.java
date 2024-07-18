ae83053fc8c4f5b3d96a25124c96c64dc2244598
isaac <fano.isaac17@gmail.com>
2024-07-18 17:31:01 +0200

    ordine dir

--- core/src/main/java/core/game/Runner.java
+++ core/src/main/java/core/game/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Runner x = new Runner();
-		System.out.println("ciaooo");
+		System.out.println(x.somma(4,6));
 	}
 
 	public int somma(int x, int y) {
